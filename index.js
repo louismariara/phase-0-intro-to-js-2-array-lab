0d48daabc69ff5e6cc8663d3df928f22b798ae66
Louis Githogori Mariara <louismariara@gmail.com>
2024-10-12 07:07:48 +0300

    my array labs

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function beforeEach() {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  }
+
+  
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+cats.unshift("Bob");
+
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(broom){
+   return [...cats, "Broom"];
+    
+}
+function prependCat(Arnold){
+   return ["Arnold", ...cats]
+}
+function removeLastCat(){
+     return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
